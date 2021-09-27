2d518826cd5e5f1293f4351a72b58d68a8fd76ef
Yemdff <91386422+Yemdff@users.noreply.github.com>
2021-09-27 18:38:36 -0400

    Update CreditsState.hx
    Changed Channel Links

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -43,8 +43,8 @@ class CreditsState extends MusicBeatState
 		['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",					'https://twitter.com/kawaisprite',		0xFF6475F3],
 		[''],
 		['VS. Boggs Development'],
-		['Shark',			'sharf',		"he owns booghost",					'https://www.youtube.com/channel/UCwWbGsScSNkz6hdlqBwuI2g',		0xaff7ee],
-		['Yem',			'yem',		"he does alot",					'https://www.youtube.com/channel/UCwWbGsScSNkz6hdlqBwuI2g',		0xaff7ee],
+		['Shark',			'sharf',		"he owns booghost",					'https://www.youtube.com/channel/UCM111exsSywUGUJ_a4LQIUw',		0xaff7ee],
+		['Yem',			'yem',		"he does alot",					'https://www.youtube.com/channel/UClWkGQwmA1IqbpVLS8cTQ1g',		0xaff7ee],
 		['Pyro',			'pyro',		"did some little stuff",					'https://www.youtube.com/channel/UC5JOVyEJesLEgyFvU9_MN1w',		0xaff7ee]
 	];
 
